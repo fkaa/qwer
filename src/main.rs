9dae2a3561773b2a24b5c35200a27866853e3bc9
Felix Kaaman <tmtu@tmtu.ee>
2021-10-20 00:29:41 +0200

    Remove unused actix leftovers

--- src/main.rs
+++ src/main.rs
@@ -41,83 +41,10 @@ use axum::{
     AddExtensionLayer,
 };
 
-/*use actix::{Actor, Addr, AsyncContext, Context, Handler, Message, StreamHandler};
-use actix_web::{
-    dev::Body, get, http::StatusCode, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
-};
-use actix_web_actors::ws;*/
-
-
 #[derive(Default)]
 struct StreamRepository {
     streams: HashMap<String, MediaFrameQueue>,
 }
-
-/*impl Handler<FindStream> for StreamRepository {
-    type Result = Option<MediaFrameQueueReceiver>;
-
-    fn handle(&mut self, msg: FindStream, _ctx: &mut Self::Context) -> Self::Result {
-        self.streams.get(&msg.0).map(|queue| queue.get_receiver())
-    }
-}*/
-
-enum WebSocketMessage {
-    Codec(u8, u8, u8),
-    Segment(bytes::Bytes, Mp4Segment),
-    Init(bytes::Bytes),
-}
-
-struct StartWebSocketMedia;
-
-/*impl Handler<WebSocketMessage> for WebSocketMediaStream {
-    type Result = ();
-
-    fn handle(&mut self, msg: WebSocketMessage, ctx: &mut Self::Context) -> Self::Result {
-        match msg {
-            WebSocketMessage::Codec(profile, constraints, level) => {
-                debug!(self.logger, "Sending codec message");
-
-                let mut framed = BytesMut::with_capacity(4);
-                framed.put_u8(0);
-                framed.put_u8(profile);
-                framed.put_u8(constraints);
-                framed.put_u8(level);
-
-                ctx.binary(framed);
-            }
-            WebSocketMessage::Init(bytes) => {
-                debug!(self.logger, "Sending init message");
-
-                let mut framed = BytesMut::with_capacity(bytes.len() + 1);
-                framed.put_u8(1);
-                framed.extend_from_slice(&bytes);
-
-                ctx.binary(framed);
-            }
-            WebSocketMessage::Segment(bytes, _segment) => {
-                let mut framed = BytesMut::with_capacity(bytes.len() + 1);
-                framed.put_u8(1);
-                framed.extend_from_slice(&bytes);
-
-                ctx.binary(framed);
-            }
-        }
-    }
-}*/
-
-/*impl StreamHandler<WebSocketMessage> for WebSocketMediaStream {
-    fn handle(&mut self, msg: WebSocketMessage, ctx: &mut Self::Context) {
-        match msg {
-            WebSocketMessage::Codec(codec) => {
-
-            }
-            WebSocketMessage::Segment(bytes, segment) => {
-
-                ctx.binary(bytes);
-            }
-        }
-    }
-}*/
 
 fn spawn_graph(logger: ContextLogger, mut graph: FilterGraph, token: &StopToken) {
     let fut = async move {
@@ -148,108 +75,6 @@ fn get_codec_from_stream(stream: &Stream) -> anyhow::Result<(u8, u8, u8)> {
         todo!()
     }
 }
-
-/*fn spawn_websocket_feeder(
-    logger: ContextLogger,
-    receiver: Receiver<anyhow::Result<StreamMessage<Mp4Metadata>>>,
-    addr: Addr<WebSocketMediaStream>,
-    token: &StopToken,
-) {
-    let fut = async move {
-        debug!(logger, "Starting websocket feeder");
-
-        while let Ok(Ok(message)) = receiver.recv().await {
-            match message {
-                StreamMessage::Start(stream) => {
-                    let (profile, constraint, level) = get_codec_from_stream(&stream).unwrap();
-                    addr.send(WebSocketMessage::Codec(profile, constraint, level))
-                        .await
-                        .unwrap();
-                }
-                StreamMessage::Frame(bytes, Mp4Metadata::Segment(segment)) => {
-                    addr.send(WebSocketMessage::Segment(bytes, segment))
-                        .await
-                        .unwrap();
-                }
-                StreamMessage::Frame(bytes, Mp4Metadata::Init) => {
-                    addr.send(WebSocketMessage::Init(bytes)).await.unwrap();
-                }
-            }
-        }
-    };
-
-    tokio::spawn(token.stop_future(fut));
-}
-
-struct WebSocketMediaStream {
-    logger: ContextLogger,
-    address: String,
-    queue: Option<MediaFrameQueueReceiver>,
-    stop_source: StopSource,
-    paused: bool,
-    sent_keyframe: bool,
-}
-
-impl WebSocketMediaStream {
-    fn new(logger: ContextLogger, address: String, queue: MediaFrameQueueReceiver) -> Self {
-        let stop_source = StopSource::new();
-
-        WebSocketMediaStream {
-            logger,
-            address,
-            queue: Some(queue),
-            stop_source,
-            paused: true,
-            sent_keyframe: false,
-        }
-    }
-
-    fn start_stream(&mut self, addr: Addr<WebSocketMediaStream>) {
-        let stop_token = self.stop_source.stop_token();
-
-        let graph_logger = self.logger.scope();
-
-        // read
-        let queue_reader = self.queue.take().unwrap();
-
-        // write
-        let (output_filter, receiver) = StreamWriteFilter::new();
-        let fmp4_filter = Box::new(FragmentedMp4WriteFilter::new(Box::new(output_filter)));
-        let write_analyzer = Box::new(FrameAnalyzerFilter::write(graph_logger.clone(), fmp4_filter));
-        let write_filter = WaitForSyncFrameFilter::new(
-            graph_logger.clone(),
-            write_analyzer,
-        );
-
-        let graph = FilterGraph::new(Box::new(queue_reader), Box::new(write_filter));
-
-        spawn_graph(graph_logger, graph, &stop_token);
-        spawn_websocket_feeder(self.logger.scope(), receiver, addr, &stop_token);
-
-        self.paused = false;
-    }
-}
-
-impl Actor for WebSocketMediaStream {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketMediaStream {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Text(txt)) if txt == "start" => {
-                self.start_stream(ctx.address());
-            }
-            Ok(ws::Message::Close(_)) => {
-                ctx.close(None);
-            }
-            _ => {
-                println!("{:?}", msg);
-            }
-        }
-    }
-}*/
-
 
 /*#[get("/http/{filename}")]
 async fn stream_video(
@@ -289,55 +114,6 @@ async fn stream_video(
     }
 }*/
 
-/*#[get("/ws/{filename}")]
-async fn ws_stream_video(
-    req: HttpRequest,
-    stream: web::Payload,
-    data: web::Data<AppData>,
-) -> HttpResponse {
-    let logger = data.logger.scope();
-
-    let path: std::path::PathBuf = req.match_info().query("filename").parse().unwrap();
-
-    let f = path.file_name().unwrap().to_str().unwrap().to_string();
-
-    let queue_receiver = data.stream_repo.send(FindStream(f.clone())).await;
-
-    debug!(logger, "Received websocket request for {} from {:?}", f, req.peer_addr());
-
-    if let Ok(Some(queue_receiver)) = queue_receiver {
-        debug!(data.logger, "Found a stream at {}", f);
-
-        let ws_stream = WebSocketMediaStream::new(logger.scope(), f, queue_receiver);
-        let response = ws::start(ws_stream, &req, stream).unwrap();
-
-        response
-    } else {
-        debug!(logger, "Did not find a stream at {}", f);
-
-        HttpResponse::NotFound().body("nope!")
-    }
-}*/
-
-/*#[derive(Debug, thiserror::Error)]
-struct HttpStreamingError(anyhow::Error);
-
-impl fmt::Display for HttpStreamingError {
-    fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(formatter, "{}", self.0)
-    }
-}
-
-impl actix_web::error::ResponseError for HttpStreamingError {
-    fn status_code(&self) -> StatusCode {
-        StatusCode::INTERNAL_SERVER_ERROR
-    }
-
-    fn error_response(&self) -> HttpResponse<Body> {
-        HttpResponse::InternalServerError().body(Body::None)
-    }
-}*/
-
 async fn authenticate_rtmp_stream(_app_name: &str, supplied_stream_key: &str) -> bool {
     std::env::var("STREAM_KEY").map(|key| key == supplied_stream_key).unwrap_or(true)
 }
@@ -374,17 +150,6 @@ async fn do_rtmp_handshake(
         .handle_input(&remaining)
         .map_err(|e| e.kind.compat())?;
 
-    /*for result in  {
-        match result {
-            ServerSessionResult::OutboundResponse(packet) => write.write(packet.bytes.into()).await?,
-            ServerSessionResult::RaisedEvent(evt) => {
-                dbg!(evt);
-            }
-            ServerSessionResult::UnhandleableMessageReceived(payload) => {},
-
-        }
-    }*/
-
     let mut authenticated = false;
     let mut r = VecDeque::new();
     let mut application_name = None;
@@ -410,11 +175,6 @@ async fn do_rtmp_handshake(
                                     .accept_request(request_id)
                                     .map_err(|e| e.kind.compat())?,
                             );
-                            /*for result in  {
-                                if let ServerSessionResult::OutboundResponse(packet) = result {
-                                    write.write(packet.bytes.into()).await?;
-                                }
-                            }*/
 
                             debug!(logger, "Accepted connection request");
 
