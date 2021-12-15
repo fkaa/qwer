e5e65a0ed7e91d0de5fc2c980d05f0caf587074d
Jokler <jokler@protonmail.com>
2021-12-15 04:32:42 +0100

    Install npm packages in workflow

--- build/package-ingest-rpm.sh
+++ build/package-ingest-rpm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # packages the output of building to a RPM package
 
-cargo build --release --target x86_64-unknown-linux-musl -p qwer-ingest &&
+set -eu
+
+cargo build --release --target x86_64-unknown-linux-musl -p qwer-ingest
 docker run --rm -it -v $(pwd):/app -v $(pwd)/artifacts:/rpmbuild qwer-ingest-build-container --entrypoint=/bin/bash
--- build/package-rpm.sh
+++ build/package-rpm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # packages the output of building to a RPM package
 
-cargo build --release --target x86_64-unknown-linux-musl -p qwer &&
-npm run build --prefix ./qw-site/frontend &&
+set -eu
+
+npm run build --prefix ./qw-site/frontend
+cargo build --release --target x86_64-unknown-linux-musl -p qwer
 docker run --rm -it -v $(pwd):/app -v $(pwd)/artifacts:/rpmbuild qwer-build-container --entrypoint=/bin/bash
