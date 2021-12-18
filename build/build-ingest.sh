5536a3b932c996d9e35b675def88f3dc18adafe2
Felix Kaaman <tmtu@tmtu.ee>
2021-12-18 18:55:10 +0100

    Fix ingest packaging script

--- build/build-ingest.sh
+++ build/build-ingest.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
 # change and setup RPM build directories
-echo "%_topdir /rpmbuild" > ~/.rpmmacros
-
-export CRATE_VERSION=$(rg '^version = "([0-9].+)"' ./qw-ingest/Cargo.toml --replace '$1')
-echo "Building RPM package for version $CRATE_VERSION"
+echo "%_topdir /app/rpmbuild" > ~/.rpmmacros
 
 rpmdev-setuptree
-
-cp ./target/x86_64-unknown-linux-musl/release/qwer-ingest build/qwer-ingest
-# tar our repository, which is used by the qwer.spec later
-tar --exclude={target,artifacts,.git,node_modules} -vpczf /rpmbuild/SOURCES/qwer-ingest.tar.gz --transform "s,^,qwer-ingest-$CRATE_VERSION/," . ./target/x86_64-unknown-linux-musl/release/qwer-ingest
-
-# build the RPM
 rpmbuild -bb ./build/qwer-ingest.spec
-
-INGEST_RTMP_ADDR=0.0.0.0:1935
-
-INGEST_WEB_ADDR=localhost:8080
-INGEST_RPC_ADDR=localhost:8081
-
-SCUFFED_RPC_ADDR=http://localhost:9081
-
-RUST_LOG=debug,h2=warn,tower=warn
--- build/build.sh
+++ build/build.sh
@@ -4,10 +4,4 @@
 echo "%_topdir /app/rpmbuild" > ~/.rpmmacros
 
 rpmdev-setuptree
-
-# cp ./target/x86_64-unknown-linux-musl/release/qwer build/qwer
-# tar our repository, which is used by the qwer.spec later
-# tar --exclude={target,artifacts,.git,node_modules} -vpczf /rpmbuild/SOURCES/qwer.tar.gz --transform "s,^,qwer-$CRATE_VERSION/," . ./target/x86_64-unknown-linux-musl/release/qwer
-
-# build the RPM
 rpmbuild -bb ./build/qwer.spec
