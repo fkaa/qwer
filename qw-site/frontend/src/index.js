1bdb2d583e0d4922979b261a1b2d769a3fa780dc
Jokler <jokler@protonmail.com>
2022-02-16 23:25:55 +0100

    Stream: Make overlay click-through and remove unused js

--- qw-site/frontend/src/index.js
+++ qw-site/frontend/src/index.js
@@ -11,14 +11,10 @@ const maxTargetBuffer = 2000;
 
 video.addEventListener("play", (e) => {
     stream.attachStream();
-
-    setPlayStatus(true);
 });
 
 video.addEventListener("pause", (e) => {
     stream.removeStream();
-
-    setPlayStatus(false);
 });
 
 function mapTargetBuffer(val) {
@@ -38,48 +34,23 @@ stream.onconnectstart = function(event) {
     console.log("Starting to connect");
 
     overlay.classList.add("hidden");
-
-    setLoadingStatus(false);
-    setLoadingProgress(null);
 };
 stream.onconnectionsuccess = function(event) {
     console.log("Connected successfully");
-
-    setLoadingStatus(true);
-    setLoadingProgress(0);
 
     clearInterval(pollInterval);
     pollInterval = setInterval(function() {
         var bufferedMs = stream.getBufferedVideoDuration() * 1000;
         var ratio = Math.min(Math.max((bufferedMs - minTargetBuffer) / (maxTargetBuffer - minTargetBuffer), 0), 1) * 100;
-
-        setLoadingProgress(ratio);
-
     }, 1000);
 };
 stream.onconnectionfail = function(event) {
     console.log("Failed to connect: " + event);
 
     clearInterval(pollInterval);
-    setPlayStatus(false);
-    setLoadingStatus(false);
-    setLoadingProgress(100);
 };
 //stream.statsContainer = container;
 stream.video = video;
-
-
-function setPlayStatus(playing) {
-    if (playing) {
-        playBox.classList.add("playing");
-        play.classList.remove("is-success");
-        play.classList.add("is-danger");
-    } else {
-        playBox.classList.remove("playing");
-        play.classList.remove("is-danger");
-        play.classList.add("is-success");
-    }
-}
 
 function copyLogs() {
     navigator.clipboard.writeText(stream.getDebugLogs());
@@ -100,7 +71,5 @@ setTimeout(() => {
         console.log("Can autoplay");
         canAutoPlay = true;
         stream.attachStream();
-
-        setPlayStatus(true);
     }
 }, 500);
